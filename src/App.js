b7c03d17099e2fdfcfcf251ea88a261ef584c248
subhrajyoti1998 <109085116+subhrajyoti1998@users.noreply.github.com>
2022-07-22 12:05:22 +0530

    Some changes for implementing All Courses, Cart, View Course Details page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import MyCartItem from './components/MyCartItem';
-import Profile from './components/Profile';
+import AllCourses from './components/AllCourses';
+import React, { useEffect, useState } from 'react'
+import CourseDetails from './components/CourseDetails';
+
 
 function App() {
+
+  const [totalPrice, setTotalPrice] = useState(0.0);
+  const [cartItems, setCartItems] = useState([]);
+  let addToCart = (item) => {
+    if ( ! cartItems.includes(item)) {
+      setCartItems(prev => [...prev, item]);
+    }
+  }
+
   return (
     <div className = "App">
-      <Header />
+      <BrowserRouter>
+        <Header />
+        <div className='container'>
+          <Routes>
+            <Route path = "/" element = {<AllCourses addElement = { addToCart }
+                                                      cartItems = { cartItems } />}/>
+            
+            <Route path = "/cart" element = {<MyCartItem setCartItems = { setCartItems }
+                                                          cartItems = { cartItems } />}/>
+            
+            <Route path = "/courseDetails" element = {<CourseDetails />}/>
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
